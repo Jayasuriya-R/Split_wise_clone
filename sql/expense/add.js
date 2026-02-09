fdb2d4ed4af430036dabfc5bd3ef61e67dce1c17
Jayasuriya R <suryaabi3801@gmail.com>
2026-02-09 22:10:31 +0530

    created the splits record in db

--- sql/expense/add.js
+++ sql/expense/add.js
@@ -1,22 +1,28 @@
 import Connection from "../connections";
 import { CREATE_NEW_EXPENSE_QUERY, CREATE_NEW_EXPENSE_SPLITS_QUERY } from "./queries"
 
-export const addNewExpense = async (expenseData,users,amount,description,loggedInUserId,groupId) => {
-   try {
-    const db = await Connection.getConnection ();
-    db.execAsync("BEGIN")
-    console.log("Transaction started")
+export const addNewExpense = async (expenseData, users, amount, description, loggedInUserId, groupId) => {
+    try {
+        const db = await Connection.getConnection();
+        db.execAsync("BEGIN")
+        console.log("Transaction started")
 
-   const expense = await addExpenseRecord(db,description,amount,loggedInUserId,groupId)
-    console.log("Expense Record created",expense)
+        const expense = await addExpenseRecord(db, description, amount, loggedInUserId, groupId)
+        console.log("Expense Record created", expense)
+        const userIds = Object.keys(expenseData).filter((uid) => +uid !== loggedInUserId)
+        for (const userId of userIds) {
+            const shareAmount = amount * (expenseData[userId] / 100)
+            const expenseSplitRecord = await addExpenseSplitRecord(db, expense, +userId, shareAmount);
+            console.log("Split Record Successfully created")
+        }
+        console.log("splits created successfully")
 
-    
-    db.execAsync("COMMIT")
-    console.log("Transaction complete")
-   } catch (error) {
-    db.execAsync("ROLLBACK")
-    console.log("Transaction failed")
-   }
+        db.execAsync("COMMIT")
+        console.log("Transaction complete")
+    } catch (error) {
+        db.execAsync("ROLLBACK")
+        console.log("Transaction failed")
+    }
 }
 
 export const addExpenseRecord = async (db, description, amount, paidBy, groupId) => {
